3d704008b405010525ff28c230c50a7a049e32bb
有猫腻 <zeng13060923171@gmail.com>
2022-08-07 22:31:49 +0800

    项目更新

--- 数据挖掘实战-情感分析（英文向）/词频-词云.py
+++ 数据挖掘实战-情感分析（英文向）/词频-词云.py
@@ -4,9 +4,8 @@ from imageio import imread
 import nltk
 import re
 from nltk.stem.snowball import SnowballStemmer  # 返回词语的原型，去掉ing等
+
 stemmer = SnowballStemmer("english")
-
-
 stop_words = []
 with open("常用英文停用词(NLP处理英文必备)stopwords.txt", 'r', encoding='utf-8') as f:
     lines = f.readlines()
@@ -27,11 +26,10 @@ def tokenize_only(text):  # 分词器，仅分词
 
 def main1():
     # 绘制词云图
-    df1 = pd.read_csv('new_twitter.csv')
-    df = df1[df1['translate'] == 'en']
+    df1 = pd.read_csv('新_第一组.csv')
     sum_cotent = []
-    df['new_content'] = df['new_content'].apply(tokenize_only)
-    df['new_content'].astype('str')
+    df1['new_推文'] = df1['new_推文'].apply(tokenize_only)
+    df1['new_content'].astype('str')
     y_data1 = list(df['new_content'])
     for d in y_data1:
         for i in d:
@@ -50,11 +48,10 @@ def main1():
         x_data.append(key)
         y_data.append(values)
 
-    df1 = pd.DataFrame()
-    df1['word'] = x_data
-    df1['counts'] = y_data
-    df1.to_csv('高频词top200.csv',encoding='utf-8-sig')
-    df.to_csv('筛选过后的文本.csv',encoding='utf-8-sig')
+    df2 = pd.DataFrame()
+    df2['word'] = x_data
+    df2['counts'] = y_data
+    df2.to_csv('第一组_高频词top200.csv',encoding='utf-8-sig')
     contents_list = " ".join(sum_cotent)
 
     # 制作词云图，collocations避免词云图中词的重复，mask定义词云图的形状，图片要有背景色
@@ -68,7 +65,7 @@ def main1():
         mask=imread('1.jpg', pilmode="RGB"))
     wc.generate(contents_list)
     # 要读取的形状的图片
-    wc.to_file("词云.jpg")
+    wc.to_file("第一组_词云.jpg")
 
 
 if __name__ == '__main__':
