2276652f27f647a8bfa154d715fad8b7fb4f8db3
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2023-02-07 16:18:56 +0800

    项目更新

--- 数据挖掘项目-人民网自动化爬取，聚类LDA分析/crwal.py
+++ 数据挖掘项目-人民网自动化爬取，聚类LDA分析/crwal.py
@@ -1,38 +1,49 @@
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 import time
 from lxml import etree
 import pandas as pd
 from urllib import parse
-
+from datetime import datetime
 
 list1 = ['肺炎','病毒','新冠','口罩']
 
-keyword = '口罩'
+keyword = '肺炎'
 keywords = parse.quote(keyword)
 
 
 def get_data():
+    a = datetime.now().timestamp()
     driver = webdriver.Chrome('chromedriver.exe')
     driver.get('http://liuyan.people.com.cn/messageSearch?keywords={}'.format(keywords))
-    # time.sleep(5)
+    #这里是控制页面的停留时间，如果到时候不好选择省份，就加延迟就好了，这里1指的是1秒，5代表5秒
+    time.sleep(5)
     # driver.find_element('xpath','//span[@class="el-checkbox__inner"]').click()
     driver.find_element('xpath','//button[@class="el-button fr el-button--danger el-button--default"]').click()
-    for i in range(2100,500001,3000):
+    for i in range(0,300001,5000):
         driver.execute_script("window.scrollBy(0,{})".format(i))
         page = driver.page_source
         soup = etree.HTML(page)
         id = soup.xpath('//span[@class="t-mr1 t-ml1"]/text()')
+        time.sleep(0.2)
         if len(id) != 0:
-            driver.find_element('xpath', '//div[@class="mordList"]').click()
-            id = soup.xpath('//span[@class="t-mr1 t-ml1"]/text()')
-            save_csv(id)
-            time.sleep(2)
+            try:
+                driver.find_element('xpath', '//div[@class="mordList"]').click()
+                id = soup.xpath('//span[@class="t-mr1 t-ml1"]/text()')
+                save_csv(id, a)
+            except:
+                driver.execute_script("window.scrollBy(0,{})".format(i))
+                page = driver.page_source
+                soup = etree.HTML(page)
+                id = soup.xpath('//span[@class="t-mr1 t-ml1"]/text()')
+                save_csv(id, a)
+                driver.close()
 
 
-def save_csv(data):
+def save_csv(data,a):
     df = pd.DataFrame()
     df['id'] = data
-    df.to_csv('{}_all_id_data.csv'.format(keyword),encoding='utf-8-sig')
+    df.to_csv('./数据/{}_{}_id_data.csv'.format(int(a),keyword),encoding='utf-8-sig')
 
 
 if __name__ == '__main__':
