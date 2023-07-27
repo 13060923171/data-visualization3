8e53e2bfb12e3b24198dd0f695025af1ddae3d9b
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2023-07-27 17:24:29 +0800

    项目更新

--- 数据挖掘实战-主题建模、信息熵/demo.py
+++ 数据挖掘实战-主题建模、信息熵/demo.py
@@ -8,14 +8,41 @@ def demo1():
         x1 = str(x).split(" ")
         x2 = x1[0] + " "+ x1[1] + " "+ x1[2] + " " + x1[-1]
         return x2
+    df['发帖数量'] = 1
+    def emtion_number(x):
+        data = x
+        new_data = data['comp_score'].value_counts()
+        x_data = [x for x in new_data.index]
+        y_data = [y for y in new_data.values]
+        d = {}
+        for x,y in zip(x_data,y_data):
+            d[x] = y
+        return d
+
+    def topic_number(x):
+        data = x
+        new_data = data['主题类型'].value_counts()
+        x_data = [x for x in new_data.index]
+        y_data = [y for y in new_data.values]
+        d = {}
+        for x,y in zip(x_data,y_data):
+            d['Topic_{}'.format(x)] = y
+        return d
 
     df['发文时间'] = df['发文时间'].apply(main1)
     df['发文时间'] = pd.to_datetime(df['发文时间'])
+    data1 = df.groupby('发文时间').apply(emtion_number)
+    data2 = df.groupby('发文时间').apply(topic_number)
+
     new_df = df.groupby('发文时间').agg('mean')
+    new_df1 = df.groupby('发文时间').agg('sum')
     df1 = pd.DataFrame()
-    df1['转发'] = new_df['转发']
-    df1['点赞'] = new_df['点赞']
-    df1['评论'] = new_df['评论']
+    df1['发帖数量'] = new_df1['发帖数量']
+    df1['情感类别'] = data1.values
+    df1['主题数量'] = data2.values
+    df1['转发'] = new_df1['转发']
+    df1['点赞'] = new_df1['点赞']
+    df1['评论'] = new_df1['评论']
     df1['熵值'] = new_df['entropy_values']
 
     plt.figure(figsize=(20,9),dpi=500)
@@ -65,12 +92,47 @@ def demo1():
 
 def demo2():
     df = pd.read_csv('./result2/new_data.csv')
+    def main1(x):
+        x1 = str(x).split(" ")
+        x2 = x1[0]
+        return x2
+
+    df['发帖数量'] = 1
+
+    def emtion_number(x):
+        data = x
+        new_data = data['comp_score'].value_counts()
+        x_data = [x for x in new_data.index]
+        y_data = [y for y in new_data.values]
+        d = {}
+        for x, y in zip(x_data, y_data):
+            d[x] = y
+        return d
+
+    def topic_number(x):
+        data = x
+        new_data = data['主题类型'].value_counts()
+        x_data = [x for x in new_data.index]
+        y_data = [y for y in new_data.values]
+        d = {}
+        for x, y in zip(x_data, y_data):
+            d['Topic_{}'.format(x)] = y
+        return d
+
+    df['发文时间'] = df['发文时间'].apply(main1)
     df['发文时间'] = pd.to_datetime(df['发文时间'])
+    data1 = df.groupby('发文时间').apply(emtion_number)
+    data2 = df.groupby('发文时间').apply(topic_number)
+
     new_df = df.groupby('发文时间').agg('mean')
+    new_df1 = df.groupby('发文时间').agg('sum')
     df1 = pd.DataFrame()
-    df1['转发'] = new_df['转发']
-    df1['点赞'] = new_df['点赞']
-    df1['评论'] = new_df['评论']
+    df1['发帖数量'] = new_df1['发帖数量']
+    df1['情感类别'] = data1.values
+    df1['主题数量'] = data2.values
+    df1['转发'] = new_df1['转发']
+    df1['点赞'] = new_df1['点赞']
+    df1['评论'] = new_df1['评论']
     df1['熵值'] = new_df['entropy_values']
 
     plt.figure(figsize=(20,9),dpi=500)
@@ -119,5 +181,5 @@ def demo2():
 
 
 if __name__ == '__main__':
-    # demo1()
+    demo1()
     demo2()
