283900bc0aab922fa72657538d7fe7b442adba39
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2023-12-11 15:51:40 +0800

    项目更新

---
+++ 数据挖掘实战-网络语义+LDA/LDA建模.py
@@ -0,0 +1,138 @@
+import pandas as pd
+# 数据处理库
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import jieba
+import jieba.analyse
+from collections import Counter
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+import itertools
+import pyLDAvis
+import pyLDAvis.gensim
+from tqdm import tqdm
+import os
+from gensim.models import LdaModel
+import gensim
+import gensim.corpora as corpora
+from gensim.models import CoherenceModel
+
+
+#LDA建模
+def lda(time_data):
+    df1 = pd.read_csv('new_data.csv')
+    # df = df1[df1['时间'] == time_data]
+    df = df1
+    train = []
+    for line in df['分词']:
+        line = [word.strip(' ') for word in line.split(' ') if len(word) >= 2]
+        train.append(line)
+
+    #构建为字典的格式
+    dictionary = corpora.Dictionary(train)
+    corpus = [dictionary.doc2bow(text) for text in train]
+
+    num_topics = 5
+
+    #LDA可视化模块
+    #构建lda主题参数
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics, random_state=111, iterations=400)
+    #读取lda对应的数据
+    data1 = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
+    #把数据进行可视化处理
+    pyLDAvis.save_html(data1, '{}_lda.html'.format(time_data))
+
+    #主题判断模块
+    list3 = []
+    list2 = []
+    #这里进行lda主题判断
+    for i in lda.get_document_topics(corpus)[:]:
+        listj = []
+        list1 = []
+        for j in i:
+            list1.append(j)
+            listj.append(j[1])
+        list3.append(list1)
+        bz = listj.index(max(listj))
+        list2.append(i[bz][0])
+
+    data = pd.DataFrame()
+    data['内容'] = df['分词']
+    data['主题概率'] = list3
+    data['主题类型'] = list2
+
+
+    #获取对应主题出现的频次
+    new_data = data['主题类型'].value_counts()
+    new_data = new_data.sort_index(ascending=True)
+    y_data1 = [y for y in new_data.values]
+
+    #主题词模块
+    word = lda.print_topics(num_words=20)
+    topic = []
+    quanzhong = []
+    list_gailv = []
+    list_gailv1 = []
+    list_word = []
+    #根据其对应的词，来获取其相应的权重
+    for w in word:
+        ci = str(w[1])
+        c1 = re.compile('\*"(.*?)"')
+        c2 = c1.findall(ci)
+        list_word.append(c2)
+        c3 = '、'.join(c2)
+
+        c4 = re.compile(".*?(\d+).*?")
+        c5 = c4.findall(ci)
+        for c in c5[::1]:
+            if c != "0":
+                gailv = str(0) + '.' + str(c)
+                list_gailv.append(gailv)
+        list_gailv1.append(list_gailv)
+        list_gailv = []
+        zt = "Topic" + str(w[0])
+        topic.append(zt)
+        quanzhong.append(c3)
+
+    #把上面权重的词计算好之后，进行保存为csv文件
+    df2 = pd.DataFrame()
+    for j,k,l in zip(topic,list_gailv1,list_word):
+        df2['{}-主题词'.format(j)] = l
+        df2['{}-权重'.format(j)] = k
+    df2.to_csv('{}_主题词分布表.csv'.format(time_data), encoding='utf-8-sig', index=False)
+
+    y_data2 = []
+    for y in y_data1:
+        number = float(y / sum(y_data1))
+        y_data2.append(float('{:0.5}'.format(number)))
+
+    df3 = pd.DataFrame()
+    df3['所属主题'] = topic
+    df3['文章数量'] = y_data1
+    df3['特征词'] = quanzhong
+    df3['主题强度'] = y_data2
+
+    plt.style.use('ggplot')
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.figure(dpi=500)
+
+    x_data = [str(x).replace('Topic', '') for x in df3['所属主题']]
+    y_data = list(df3['文章数量'])
+    plt.pie(y_data, labels=x_data, startangle=0, autopct='%1.2f%%')
+    plt.title('{}_theme strength'.format(time_data))
+    # 添加图例
+    plt.legend(x_data, loc='lower right')
+    plt.tight_layout()
+    plt.savefig('{}_theme strength.png'.format(time_data))
+
+
+if __name__ == '__main__':
+    # list_data = [2020,2021,2022,2023]
+    # for l in tqdm(list_data):
+    #     lda(l)
+    lda('总体')
+
---
+++ 数据挖掘实战-网络语义+LDA/网络语义.py
@@ -0,0 +1,184 @@
+import codecs
+import networkx as nx
+from collections import Counter
+from scipy.sparse import coo_matrix
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+
+
+def demo(time_data):
+    df1 = pd.read_csv('new_data.csv')
+    # df = df1[df1['时间'] == time_data]
+    df = df1
+    d = {}
+    list_text = []
+    for t in df['分词']:
+        t1 = str(t).split(" ")
+        # 把数据分开
+        for i in t1:
+            # 添加到列表里面
+            list_text.append(i)
+            d[i] = d.get(i,0)+1
+
+    ls = list(d.items())
+    ls.sort(key=lambda x:x[1],reverse=True)
+    x_data = []
+    y_data = []
+    for key,values in ls[:100]:
+        x_data.append(key)
+        y_data.append(values)
+
+    data = pd.DataFrame()
+    data['word'] = x_data
+    data['counts'] = y_data
+
+    # 导入停用词列表
+
+    def fenci():
+        f = open('{}_fenci.txt'.format(time_data), 'w', encoding='utf-8-sig')
+        for line in df['分词']:
+            line1 = str(line).split(' ')
+            # 计算关键词
+            c = Counter()
+            for x in line1:
+                if x in x_data:
+                    c[x] += 1
+            output = ""
+            for (k, v) in c.most_common():
+                output += k + " "
+
+            f.write(output + "\n")
+        else:
+            f.close()
+
+    fenci()
+
+    word = []  # 记录关键词
+    f = open("{}_fenci.txt".format(time_data), encoding='utf-8')
+    line = f.readline()
+    while line:
+        # print line
+        line = line.replace("\n", "")  # 过滤换行
+        line = line.strip('\n')
+        for n in line.split(' '):
+            # print n
+            if n not in word:
+                word.append(n)
+        line = f.readline()
+    f.close()
+
+    word_vector = coo_matrix((len(word), len(word)), dtype=np.int8).toarray()
+
+    f = open("{}_fenci.txt".format(time_data), encoding='utf-8')
+    line = f.readline()
+    while line:
+        line = line.replace("\n", "")  # 过滤换行
+        line = line.strip('\n')  # 过滤换行
+        nums = line.split(' ')
+        # 循环遍历关键词所在位置 设置word_vector计数
+        i = 0
+        j = 0
+        while i < len(nums):  # ABCD共现 AB AC AD BC BD CD加1
+            j = i + 1
+            w1 = nums[i]  # 第一个单词
+            while j < len(nums):
+                w2 = nums[j]  # 第二个单词
+                # 从word数组中找到单词对应的下标
+                k = 0
+                n1 = 0
+                while k < len(word):
+                    if w1 == word[k]:
+                        n1 = k
+                        break
+                    k = k + 1
+                # 寻找第二个关键字位置
+                k = 0
+                n2 = 0
+                while k < len(word):
+                    if w2 == word[k]:
+                        n2 = k
+                        break
+                    k = k + 1
+
+                # 重点: 词频矩阵赋值 只计算上三角
+                if n1 <= n2:
+                    word_vector[n1][n2] = word_vector[n1][n2] + 1
+                else:
+                    word_vector[n2][n1] = word_vector[n2][n1] + 1
+                j = j + 1
+            i = i + 1
+        # 读取新内容
+        line = f.readline()
+    f.close()
+
+    words = codecs.open("{}_word_node.txt".format(time_data), "w", "utf-8")
+    i = 0
+    while i < len(word):
+        student1 = word[i]
+        j = i + 1
+        while j < len(word):
+            student2 = word[j]
+            if word_vector[i][j] > 0:
+                words.write(student1 + " " + student2 + " "
+                            + str(word_vector[i][j]) + "\r\n")
+            j = j + 1
+        i = i + 1
+    words.close()
+
+    """ 第四步:图形生成 """
+    with open('{}_word_node.txt'.format(time_data), 'r', encoding='utf-8') as f:
+        content = f.readlines()
+    list_word1 = []
+    list_word2 = []
+    list_weight = []
+    for i in content:
+        c = i.strip('\n').split(" ")
+        list_word1.append(c[0])
+        list_word2.append(c[1])
+        list_weight.append(c[2])
+
+    data1 = pd.DataFrame()
+    data1['word1'] = list_word1
+    data1['word2'] = list_word2
+    data1['weight'] = list_weight
+    data1['weight'] = data1['weight'].astype(int)
+    data1 = data1.sort_values(by=['weight'], ascending=False)
+    data1 = data1.dropna(how='any', axis=1)
+    new_data = data1.iloc[:300]
+
+    A = []
+    B = []
+    for w1, w2 in tqdm(zip(new_data['word1'], new_data['word2'])):
+        if w1 != "" and w2 != "":
+            A.append(w1)
+            B.append(w2)
+    elem_dic = tuple(zip(A, B))
+    # 创建一个空的无向图。即创建了一个称为G的图对象，用于保存文本数据的节点和边信息。
+    G = nx.Graph()
+    # 向图G中添加节点和边。这里的list(elem_dic)表示将elem_dic字典中的元素列表作为图的边。其中elem_dic字典中存储着文本数据的节点和边信息。
+    G.add_edges_from(list(elem_dic))
+    # 设置图像中使用中文字体，以避免出现显示中文乱码的情况。这里将字体设置为SimHei，使用sans-serif字体族。
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['font.family'] = 'sans-serif'
+    # 设置图像的大小，其中figsize参数设置图像的宽度和高度。
+    plt.figure(figsize=(16, 9), dpi=500)
+    # 确定节点布局。这里使用了一种称为spring layout的布局算法，相当于在二维空间中对节点进行排列。iterations参数指定了进行节点排列的迭代次数。
+    pos = nx.spring_layout(G, iterations=10)
+    # 绘制节点。其中alpha参数设置节点的透明度，node_size参数设置节点的大小。
+    nx.draw_networkx_nodes(G, pos, alpha=0.7, node_size=800)
+    # 绘制边。其中width参数设置边的宽度，alpha参数设置边的透明度，edge_color参数设置边的颜色。
+    nx.draw_networkx_edges(G, pos, width=0.5, alpha=0.8, edge_color='g')
+    # 添加标签。其中font_family参数指定图像中使用sans-serif字体族，alpha参数设置节点标签的透明度，font_size参数设置归纳节点标签的字体大小。
+    nx.draw_networkx_labels(G, pos, font_family='sans-serif', alpha=1, font_size='10')
+    plt.title("{}_co-occurrence semantics".format(time_data))
+    plt.savefig('{}_共现语义.png'.format(time_data))
+    plt.show()
+
+
+if __name__ == '__main__':
+    # list_data = [2020,2021,2022,2023]
+    # for l in tqdm(list_data):
+    #     demo(l)
+    demo('总体')
