773bae57ab48a2d05abb73755c89f8edcac9c3ac
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2024-01-15 15:57:23 +0800

    项目更新

---
+++ 数据挖掘实战-中文数据集，lda好评、差评、中立研究/数据处理.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import re
+import jieba
+import jieba.posseg as pseg
+from snownlp import SnowNLP
+
+# 去掉重复行以及空值
+df = pd.read_excel('product_id.xlsx',sheet_name='筛选后')
+
+
+# 导入停用词列表
+stop_words = []
+with open("stopwords_cn.txt", 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        stop_words.append(line.strip())
+
+
+#去掉标点符号，以及机械压缩
+def preprocess_word(word):
+    word1 = str(word)
+    # word1 = re.sub(r'转发微博', '', word1)
+    word1 = re.sub(r'#\w+#', '', word1)
+    word1 = re.sub(r'【.*?】', '', word1)
+    word1 = re.sub(r'@[\w]+', '', word1)
+    word1 = re.sub(r'[a-zA-Z]', '', word1)
+    word1 = re.sub(r'\.\d+', '', word1)
+    return word1
+
+def emjio_tihuan(x):
+    x1 = str(x)
+    x2 = re.sub('(\[.*?\])', "", x1)
+    x3 = re.sub(r'@[\w\u2E80-\u9FFF]+:?|\[\w+\]', '', x2)
+    x4 = re.sub(r'\n', '', x3)
+    return x4
+
+# 判断是否为中文
+def is_all_chinese(strs):
+    for _char in strs:
+        if not '\u4e00' <= _char <= '\u9fa5':
+            return False
+    return True
+
+
+# 定义机械压缩函数
+def yasuo(st):
+    for i in range(1, int(len(st) / 2) + 1):
+        for j in range(len(st)):
+            if st[j:j + i] == st[j + i:j + 2 * i]:
+                k = j + i
+                while st[k:k + i] == st[k + i:k + 2 * i] and k < len(st):
+                    k = k + i
+                st = st[:j] + st[k:]
+    return st
+
+
+def get_cut_words(content_series):
+    try:
+        # 对文本进行分词和词性标注
+        words = pseg.cut(content_series)
+        # 保存名词和形容词的列表
+        nouns_and_adjs = []
+        # 逐一检查每个词语的词性，并将名词和形容词保存到列表中
+        for word, flag in words:
+            if word not in stop_words and len(word) >= 2 and is_all_chinese(word) == True:
+                # 如果是名词或形容词，就将其保存到列表中
+                nouns_and_adjs.append(word)
+        if len(nouns_and_adjs) != 0:
+            return ' '.join(nouns_and_adjs)
+        else:
+            return np.NAN
+    except:
+        return np.NAN
+
+def process_date(x):
+    x1 = str(x)
+    x1 = x1.split(" ")
+    return x1[0]
+
+df['发布时间'] = df['发布时间'].apply(process_date)
+df['发布时间'] = pd.to_datetime(df['发布时间'])
+df.index = df['发布时间']
+df1 = df['2023-09-01':]
+
+df1['评论'] = df1['评论'].apply(preprocess_word)
+df1['评论'] = df1['评论'].apply(emjio_tihuan)
+df1.dropna(subset=['评论'], axis=0,inplace=True)
+df1['评论'] = df1['评论'].apply(yasuo)
+df1['分词'] = df1['评论'].apply(get_cut_words)
+new_df = df1.dropna(subset=['分词'], axis=0)
+new_df = new_df.drop_duplicates(subset=['分词'])
+new_df.to_csv('new_data.csv', encoding='utf-8-sig')
+
