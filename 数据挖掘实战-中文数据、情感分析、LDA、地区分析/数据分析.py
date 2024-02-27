ec4654f6573e891d921380b1b36a6c84c12ff32d
v_yuhaozeng(曾宇皓) <v_yuhaozeng@tencent.com>
2024-02-27 17:10:43 +0800

    项目更新

--- 数据挖掘实战-中文数据、情感分析、LDA、地区分析/数据分析.py
+++ 数据挖掘实战-中文数据、情感分析、LDA、地区分析/数据分析.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import defaultdict
 from pyecharts import options as opts
-from pyecharts.charts import Geo
+from pyecharts.charts import Map
 
 
 def emotion_analysis():
@@ -52,7 +52,7 @@ def map_analyze():
 
     ls = list(d1.items())
     ls.sort(key=lambda x:x[1],reverse=True)
-
+    print(ls)
     data = []
     provinces = ['安徽', '澳门', '北京', '重庆', '福建', '甘肃', '广东', '广西', '贵州', '海南', '河北', '黑龙江', '河南', '湖北', '湖南', '江苏', '江西',
                  '吉林', '辽宁', '内蒙古', '宁夏', '青海', '山东', '上海', '山西', '陕西', '四川', '台湾', '天津', '西藏', '香港', '新疆', '云南', '浙江']
@@ -60,6 +60,12 @@ def map_analyze():
     y_data = []
     for key,values in ls:
         if key in provinces:
+            if '北京' == key or '重庆' == key or '上海' == key or '天津' == key:
+                key = str(key) + '市'
+            elif '广西' != key or '新疆' != key or '宁夏' != key or '西藏' != key:
+                key = str(key) + '省'
+            else:
+                key = str(key).replace('广西','广西壮族自治区').replace('新疆','新疆维吾尔自治区').replace('宁夏','宁夏回族自治区').replace('西藏','西藏自治区')
             data.append((key,int(values)))
             x_data.append(key)
             y_data.append(values)
@@ -71,16 +77,27 @@ def map_analyze():
     df.to_csv('地区分析.csv',encoding='utf-8-sig')
 
     c = (
-        Geo()
-            .add_schema(maptype="china")
-            .add("中国地图", data)
+        Map()
+            .add("中国地图", data, "china")
             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
             .set_global_opts(
             title_opts=opts.TitleOpts(title="地域分析"),
-            visualmap_opts=opts.VisualMapOpts(is_piecewise=True,max_=int(y_data[0])),
+            visualmap_opts=opts.VisualMapOpts(max_=200, is_piecewise=True),
         )
             .render("地域分析.html")
     )
+
+    # c = (
+    #     Geo()
+    #         .add_schema(maptype="china")
+    #         .add("中国地图", data)
+    #         .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+    #         .set_global_opts(
+    #         title_opts=opts.TitleOpts(title="地域分析"),
+    #         visualmap_opts=opts.VisualMapOpts(is_piecewise=True,max_=int(y_data[0])),
+    #     )
+    #         .render("地域分析.html")
+    # )
 
 
 def process_time():
@@ -179,29 +196,29 @@ def score(area):
     plt.savefig('{}-评分趋势.png'.format(area))
 
 if __name__ == '__main__':
-    emotion_analysis()
+    # emotion_analysis()
     map_analyze()
-    process_time()
-    df = pd.DataFrame()
-    new_df3 = pd.DataFrame()
-    new_df3['地区分布'] = ['地区分布']
-    new_df3['产品线路'] = ['产品线路']
-    new_df3['出游总人数'] = ['出游总人数']
-    new_df3['平均单价'] = ['平均单价']
-    new_df3['整体好评率'] = ['整体好评率']
-    new_df3['好评占比'] =['好评占比']
-    new_df3['差评占比'] = ['差评占比']
-    new_df3.to_csv('区域分析.csv', encoding='utf-8-sig', index=False, header=False, mode='w')
-    new_df4 = pd.DataFrame()
-    new_df4['地区分布'] = ['地区分布']
-    new_df4['其他出游'] = ['其他出游']
-    new_df4['单独旅行'] = ['单独旅行']
-    new_df4['家庭亲子'] = ['家庭亲子']
-    new_df4['情侣夫妻'] = ['情侣夫妻']
-    new_df4['朋友出游'] = ['朋友出游']
-    new_df4.to_csv('出行类型分析.csv', encoding='utf-8-sig', index=False, header=False, mode='w')
-    list_area = ['东南亚','欧美','日韩']
-    for l in list_area:
-        myd_data(l)
-        travel_type(l)
-        score(l)
+    # process_time()
+    # df = pd.DataFrame()
+    # new_df3 = pd.DataFrame()
+    # new_df3['地区分布'] = ['地区分布']
+    # new_df3['产品线路'] = ['产品线路']
+    # new_df3['出游总人数'] = ['出游总人数']
+    # new_df3['平均单价'] = ['平均单价']
+    # new_df3['整体好评率'] = ['整体好评率']
+    # new_df3['好评占比'] =['好评占比']
+    # new_df3['差评占比'] = ['差评占比']
+    # new_df3.to_csv('区域分析.csv', encoding='utf-8-sig', index=False, header=False, mode='w')
+    # new_df4 = pd.DataFrame()
+    # new_df4['地区分布'] = ['地区分布']
+    # new_df4['其他出游'] = ['其他出游']
+    # new_df4['单独旅行'] = ['单独旅行']
+    # new_df4['家庭亲子'] = ['家庭亲子']
+    # new_df4['情侣夫妻'] = ['情侣夫妻']
+    # new_df4['朋友出游'] = ['朋友出游']
+    # new_df4.to_csv('出行类型分析.csv', encoding='utf-8-sig', index=False, header=False, mode='w')
+    # list_area = ['东南亚','欧美','日韩']
+    # for l in list_area:
+    #     myd_data(l)
+    #     travel_type(l)
+    #     score(l)
