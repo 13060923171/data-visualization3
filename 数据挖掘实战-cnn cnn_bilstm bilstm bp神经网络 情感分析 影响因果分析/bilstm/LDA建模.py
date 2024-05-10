62f74650c61b416960da830ac3a43ad8f903f479
有猫腻妖 <960751327@qq.com>
2024-05-10 15:27:56 +0800

    项目更新

---
+++ 数据挖掘实战-cnn cnn_bilstm bilstm bp神经网络 情感分析 影响因果分析/bilstm/LDA建模.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+# 数据处理库
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')  # 使用Agg后端
+
+import re
+import os
+import itertools
+from tqdm import tqdm
+from collections import Counter
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+import pyLDAvis
+import pyLDAvis.gensim
+
+import gensim
+import gensim.corpora as corpora
+from gensim.models import LdaModel
+from gensim.models import CoherenceModel
+
+
+#LDA建模
+def lda(df,emotion):
+    train = []
+    stop_word = []
+    with open("../stopwords_cn.txt", 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            stop_word.append(line.strip())
+    for line in df['content']:
+        line = [str(word).strip(' ') for word in line.split(' ') if len(word) >= 2 and word not in stop_word]
+        train.append(line)
+
+    #构建为字典的格式
+    dictionary = corpora.Dictionary(train)
+    corpus = [dictionary.doc2bow(text) for text in train]
+    
+    if not os.path.exists("./{}".format(emotion)):
+        os.mkdir("./{}".format(emotion))
+
+
+    # 困惑度模块
+    x_data = []
+    y_data = []
+    z_data = []
+    for i in tqdm(range(2, 15)):
+        x_data.append(i)
+        lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=dictionary,num_topics=i)
+        # 困惑度计算
+        perplexity = lda_model.log_perplexity(corpus)
+        y_data.append(perplexity)
+        # 一致性计算
+        coherence_model_lda = CoherenceModel(model=lda_model, texts=train, dictionary=dictionary, coherence='c_v')
+        coherence = coherence_model_lda.get_coherence()
+        z_data.append(coherence)
+
+    # 绘制困惑度和一致性折线图
+    fig = plt.figure(figsize=(15, 5))
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    matplotlib.rcParams['axes.unicode_minus'] = False
+
+    # 绘制困惑度折线图
+    ax1 = fig.add_subplot(1, 2, 1)
+    plt.plot(x_data, y_data, marker="o")
+    plt.title("perplexity_values")
+    plt.xlabel('num topics')
+    plt.ylabel('perplexity score')
+    #绘制一致性的折线图
+    ax2 = fig.add_subplot(1, 2, 2)
+    plt.plot(x_data, z_data, marker="o")
+    plt.title("coherence_values")
+    plt.xlabel("num topics")
+    plt.ylabel("coherence score")
+
+    plt.savefig('./{}/困惑度和一致性.png'.format(emotion))
+
+    #将上面获取的数据进行保存
+    df5 = pd.DataFrame()
+    df5['主题数'] = x_data
+    df5['困惑度'] = y_data
+    df5['一致性'] = z_data
+    df5.to_csv('./{}/困惑度和一致性.csv'.format(emotion),encoding='utf-8-sig',index=False)
+
+    optimal_z = max(z_data)
+    optimal_z_index = z_data.index(optimal_z)
+    best_topic_number = x_data[optimal_z_index]
+
+    if emotion == 'neg':
+        num_topics = 3
+    else:
+        num_topics = best_topic_number
+
+    #LDA可视化模块
+    #构建lda主题参数
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics, random_state=111, iterations=400)
+    #读取lda对应的数据
+    data1 = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
+    #把数据进行可视化处理
+    pyLDAvis.save_html(data1, './{}/lda.html'.format(emotion))
+
+    #主题判断模块
+    list3 = []
+    list2 = []
+    #这里进行lda主题判断
+    for i in lda.get_document_topics(corpus)[:]:
+        listj = []
+        list1 = []
+        for j in i:
+            list1.append(j)
+            listj.append(j[1])
+        list3.append(list1)
+        bz = listj.index(max(listj))
+        list2.append(i[bz][0])
+
+
+    df['主题概率'] = list3
+    df['主题类型'] = list2
+
+    df.to_csv('./{}/lda_data.csv'.format(emotion),encoding='utf-8-sig',index=False)
+
+    data = df
+    #获取对应主题出现的频次
+    new_data = data['主题类型'].value_counts()
+    new_data = new_data.sort_index(ascending=True)
+    y_data1 = [y for y in new_data.values]
+
+    #主题词模块
+    word = lda.print_topics(num_words=20)
+    topic = []
+    quanzhong = []
+    list_gailv = []
+    list_gailv1 = []
+    list_word = []
+    #根据其对应的词，来获取其相应的权重
+    for w in word:
+        ci = str(w[1])
+        c1 = re.compile('\*"(.*?)"')
+        c2 = c1.findall(ci)
+        list_word.append(c2)
+        c3 = '、'.join(c2)
+
+        c4 = re.compile(".*?(\d+).*?")
+        c5 = c4.findall(ci)
+        for c in c5[::1]:
+            if c != "0":
+                gailv = str(0) + '.' + str(c)
+                list_gailv.append(gailv)
+        list_gailv1.append(list_gailv)
+        list_gailv = []
+        zt = "Topic" + str(w[0])
+        topic.append(zt)
+        quanzhong.append(c3)
+
+    #把上面权重的词计算好之后，进行保存为csv文件
+    df2 = pd.DataFrame()
+    for j,k,l in zip(topic,list_gailv1,list_word):
+        df2['{}-主题词'.format(j)] = l
+        df2['{}-权重'.format(j)] = k
+    df2.to_csv('./{}/主题词分布表.csv'.format(emotion), encoding='utf-8-sig', index=False)
+
+    y_data2 = []
+    for y in y_data1:
+        number = float(y / sum(y_data1))
+        y_data2.append(float('{:0.5}'.format(number)))
+
+    df1 = pd.DataFrame()
+    df1['所属主题'] = topic
+    df1['文章数量'] = y_data1
+    df1['特征词'] = quanzhong
+    df1['主题强度'] = y_data2
+    df1.to_csv('./{}/特征词.csv'.format(emotion),encoding='utf-8-sig',index=False)
+
+
+if __name__ == '__main__':
+    list_emotion = ['neg','pos']
+    for e in list_emotion:
+        df = pd.read_excel(f'{e}.xlsx')
+        data = df[df['情感type'] == e]
+        lda(data,e)
+
+
+
+
--- 数据挖掘实战-cnn cnn_bilstm bilstm bp神经网络 情感分析 影响因果分析/bilstm/可视化.py
+++ 数据挖掘实战-cnn cnn_bilstm bilstm bp神经网络 情感分析 影响因果分析/bilstm/可视化.py
@@ -8,11 +8,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')  # 使用Agg后端
 
-import random
-from PIL import Image
-from matplotlib.pyplot import imread
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+# import random
+# from PIL import Image
+# from matplotlib.pyplot import imread
+# from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+import pyecharts.options as opts
+from pyecharts.charts import Bar
+from pyecharts.charts import WordCloud
+
+# 导入停用词列表
+stop_words = ['满意','不满意']
+with open("../stopwords_cn.txt", 'r', encoding='utf-8-sig') as f:
+    lines = f.readlines()
+    for line in lines:
+        stop_words.append(line.strip())
 
 def word(df,emotion):
     d = {}
@@ -21,6 +31,7 @@ def word(df,emotion):
         # 把数据分开
         t = str(t).split(" ")
         for i in t:
+            if i not in stop_words:
                 # 添加到列表里面
                 list_text.append(i)
                 d[i] = d.get(i,0)+1
@@ -29,7 +40,7 @@ def word(df,emotion):
     ls.sort(key=lambda x:x[1],reverse=True)
     x_data = []
     y_data = []
-    for key,values in ls:
+    for key,values in ls[:200]:
         x_data.append(key)
         y_data.append(values)
 
@@ -37,44 +48,95 @@ def word(df,emotion):
     data['word'] = x_data
     data['counts'] = y_data
 
-    data.to_csv('{}-高频词.csv'.format(emotion),encoding='utf-8-sig',index=False)
-    x_data.reverse()
-    y_data.reverse()
+    data.to_csv('{}-高频词Top200.csv'.format(emotion),encoding='utf-8-sig',index=False)
 
-    plt.figure(figsize=(12, 9),dpi=500)
-    plt.barh(x_data[:20], y_data[:20])
-    plt.rcParams['font.sans-serif'] = ['SimHei']
-    plt.title("{}-高频词Top20".format(emotion))
-    plt.xlabel("频次")
-    plt.savefig('{}-高频词Top20'.format(emotion))
-    stopwords = ['车子','感觉','特别','款车']
-    def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-        return "hsl(191, 0%, 99%)"
 
-    # 读取背景图片
-    background_Image = np.array(Image.open('../image.png'))
-    text = ' '.join(list_text)
-    wc = WordCloud(
-        stopwords=stopwords,
-        collocations=False,
-        font_path='simhei.ttf',  # 中文需设置路径
-        margin=1,  # 页面边缘
-        mask=background_Image,
-        scale=10,
-        # max_words=100,  # 最多词个数
-        random_state=42,
-        width=900,
-        height=600,
-        min_font_size=4,
-        max_font_size=80,
-        background_color='SlateGray',  # 背景颜色
-        color_func=color_func #字体颜色
+    # def color_func(word, font_size, position, orientation, random_state=None,
+    #                **kwargs):
+    #     return "hsl({}, {}%, {}%)".format(random.randint(240, 250),random.randint(20, 50),random.randint(50, 80))
+    #
+    # # 读取背景图片
+    # background_Image = np.array(Image.open('../image.jpg'))
+    # text = ' '.join(list_text)
+    # wc = WordCloud(
+    #     collocations=False,  # 禁用词组
+    #     font_path='simhei.ttf',  # 中文字体路径
+    #     margin=3,  # 词云图边缘宽度
+    #     mask=background_Image,  # 背景图形
+    #     scale=5,  # 放大倍数
+    #     max_words=200,  # 最多词个数
+    #     random_state=42,  # 随机状态
+    #     width=800,  # 图片宽度
+    #     height=600,  # 图片高度
+    #     min_font_size=10,  # 最小字体大小
+    #     max_font_size=80,  # 最大字体大小
+    #     background_color='white',  # 背景颜色
+    #     color_func=color_func  # 字体颜色函数
+    # )
+    # # 生成词云
+    # wc.generate_from_text(text)
+    # # 存储图像
+    # wc.to_file("{}-top200-词云图.png".format(emotion))
 
+
+def word1(emotion):
+    new_df = pd.read_csv('{}-高频词Top200.csv'.format(emotion),encoding='gbk')
+    df1 = new_df.sort_values(by=['counts'],ascending=False)
+    x_data1 = list(df1['word'][:30])
+    y_data1 = list(df1['counts'][:30])
+
+    c = (
+        Bar()
+            .add_xaxis(x_data1)
+            .add_yaxis(f"{emotion}", y_data1, label_opts=opts.LabelOpts(is_show=False))
+            .set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=65)),
+            title_opts=opts.TitleOpts(title=f"{emotion}-高频词Top30"))
+            .render(f"{emotion}-高频词Top30.html")
     )
-    # 生成词云
-    wc.generate_from_text(text)
-    # 存储图像
-    wc.to_file("{}-top100-词云图.png".format(emotion))
+
+
+    data = []
+    for key, values in zip(df1['word'], df1['counts']):
+        data.append((str(key),int(values)))
+    if emotion == 'pos':
+        # 创造词云图实例时应用遮罩图片
+        wordcloud = (
+            WordCloud()
+                .add(
+                series_name="热点分析",
+                data_pair=data,
+                word_size_range=[8,102],
+                shape='circle',  # 这里是错误的示范，正确的做法见下方
+            )
+                .set_global_opts(
+                title_opts=opts.TitleOpts(
+                    title=f"{emotion}-top200-词云展示",
+                    title_textstyle_opts=opts.TextStyleOpts(font_size=20)
+                ),
+                tooltip_opts=opts.TooltipOpts(is_show=True),
+            )
+                .render(f"{emotion}-top200-词云图.html")
+        )
+    else:
+        # 创造词云图实例时应用遮罩图片
+        wordcloud = (
+            WordCloud()
+                .add(
+                series_name="热点分析",
+                data_pair=data,
+                word_size_range=[8,88],
+                shape='triangle',  # 这里是错误的示范，正确的做法见下方
+            )
+                .set_global_opts(
+                title_opts=opts.TitleOpts(
+                    title=f"{emotion}-top200-词云展示",
+                    title_textstyle_opts=opts.TextStyleOpts(font_size=16)
+                ),
+                tooltip_opts=opts.TooltipOpts(is_show=True),
+            )
+                .render(f"{emotion}-top200-词云图.html")
+        )
 
 
 if __name__ == '__main__':
@@ -82,7 +144,8 @@ if __name__ == '__main__':
     for e in list_emotion:
         df = pd.read_excel('new_bi_lstm.xlsx')
         data = df[df['情感type'] == e]
-        word(data,e)
+        # word(data,e)
+        word1(e)
 
 
 
