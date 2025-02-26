28a7b49a7292862dd416f0c89b23edd423449967
有猫腻妖 <960751327@qq.com>
2025-02-26 09:51:01 +0800

    项目更新

---
+++ 数据挖掘实战-LDA分析 情感分析 中文数据/不背单词/数据分析.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+# 数据处理库
+import matplotlib
+import matplotlib.pyplot as plt
+from pyecharts.charts import Bar
+import pyecharts.options as opts
+from pyecharts.charts import Pie
+
+matplotlib.use('Agg')  # 使用Agg后端
+
+df1 = pd.read_excel('./不背单词-lda/不背单词-lda_data(二级维度-技术设计).xlsx')
+df1['二级-主题类型'] = df1['二级-主题类型'].replace(0,'屏幕设计').replace(1,'交互性').replace(2,'目标导向').replace(3,'信息演示')
+df2 = pd.read_excel('./不背单词-lda/不背单词-lda_data(二级维度-面向教育).xlsx')
+df2['二级-主题类型'] = df2['二级-主题类型'].replace(0,'连接未来学习').replace(1,'学习材料的水平').replace(2,'内容的严格性').replace(3,'21世纪技能')
+
+df6 = pd.concat([df1,df2],axis=0)
+
+df3 = pd.read_excel('./不背单词-lda/特征维度(二级维度-技术设计).xlsx')
+df3['所属主题'] = df3['所属主题'].replace('Topic0','屏幕设计').replace('Topic1','交互性').replace('Topic2','目标导向').replace('Topic3','信息演示')
+df4 = pd.read_excel('./不背单词-lda/特征维度(二级维度-面向教育).xlsx')
+df4['所属主题'] = df4['所属主题'].replace('Topic0','连接未来学习').replace('Topic1','学习材料的水平').replace('Topic2','内容的严格性').replace('Topic3','21世纪技能')
+
+df5 = pd.read_excel('./不背单词-lda/特征维度(一级维度).xlsx')
+df5['所属主题'] = df5['所属主题'].replace('Topic0','技术设计').replace('Topic1','面向教育')
+
+list_name = ['技术设计','面向教育']
+
+def pie1(data,name):
+    x_data = [x for x in data['所属主题']]
+    y_data = [y for y in data['文本数量']]
+
+    data_pair = [list(z) for z in zip(x_data, y_data)]
+    data_pair.sort(key=lambda x: x[1])
+
+    c = (
+        Pie()
+            .add(
+            "",
+            data_pair,
+            center=["35%", "50%"],
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title=f"APP-{name}"),
+            legend_opts=opts.LegendOpts(pos_left="15%"),
+        )
+            .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {d}%"))
+            .render(f"APP-{name}.html")
+    )
+
+
+list_df = [df3,df4]
+
+for d,n in zip(list_df,list_name):
+    pie1(d,n)
+
+data = pd.DataFrame()
+data['特征二级维度'] = df3['所属主题']
+data['特征二级维度关注度'] = df3['维度关注度']
+data['特征二级维度-关注度内部占比'] = df3['关注度内部占比']
+data['特征一级维度'] = '{}'.format(df5['所属主题'].values[0])
+data['特征一级维度关注度'] = '{}'.format(df5['维度关注度'].values[0])
+data['特征一级维度-关注度内部占比'] = '{}'.format(df5['内部占比'].values[0])
+
+
+data1 = pd.DataFrame()
+data1['特征二级维度'] = df4['所属主题']
+data1['特征二级维度关注度'] = df4['维度关注度']
+data1['特征二级维度-关注度内部占比'] = df4['关注度内部占比']
+data1['特征一级维度'] = '{}'.format(df5['所属主题'].values[1])
+data1['特征一级维度关注度'] = '{}'.format(df5['维度关注度'].values[1])
+data1['特征一级维度-关注度内部占比'] = '{}'.format(df5['内部占比'].values[1])
+
+new_data = pd.concat([data,data1],axis=0)
+new_data = new_data[['特征一级维度','特征一级维度关注度','特征一级维度-关注度内部占比','特征二级维度','特征二级维度关注度','特征二级维度-关注度内部占比']]
+
+new_data.to_excel('特征数据.xlsx',index=False)
+
+def data_process(df,t):
+    df1 = df[df['二级-主题类型'] == t]
+    new_df = df1['sentiment'].value_counts()
+    d = {}
+    for x,y in zip(new_df.index,new_df.values):
+        d[x] = y
+    try:
+        pos = d['正面']
+    except:
+        pos = 0
+
+    try:
+        neg = d['负面']
+    except:
+        neg = 0
+
+    return t,pos,neg
+
+
+list_name = []
+list_pos = []
+list_neg = []
+new_df = df6['二级-主题类型'].value_counts()
+type1 = [x for x in new_df.index]
+for t in type1:
+    t,pos,neg = data_process(df6,t)
+    list_name.append(t)
+    list_pos.append(int(pos))
+    list_neg.append(int(neg))
+
+
+
+
+# 创建堆叠条形图
+bar = (
+    Bar()
+    .add_xaxis(list_name)
+    # 添加正向数据堆叠层（颜色绿色）
+    .add_yaxis("正向", list_pos, stack="stack1", color="#4DB6AC")
+    # 添加负向数据堆叠层（颜色红色）
+    .add_yaxis("负向", list_neg, stack="stack1", color="#c23531")
+    .reversal_axis()
+    # 全局配置
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="APP-各特征维度情感分析"),
+        xaxis_opts=opts.AxisOpts(name="特征"),
+        yaxis_opts=opts.AxisOpts(name="数量"),
+        legend_opts=opts.LegendOpts(pos_top="5%")
+    )
+)
+
+# 生成HTML文件（默认保存到当前目录）
+bar.render("APP-各特征维度情感分析.html")
---
+++ 数据挖掘实战-LDA分析 情感分析 中文数据/扇贝/lda分析-一级维度.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import numpy as np
+# 数据处理库
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')  # 使用Agg后端
+
+import re
+import os
+import itertools
+from tqdm import tqdm
+from collections import Counter
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+import pyLDAvis
+import pyLDAvis.gensim 
+
+import gensim
+import gensim.corpora as corpora
+from gensim.models import LdaModel
+from gensim.models import CoherenceModel
+
+
+# LDA建模
+def lda(df):
+    train = []
+    stop_word = []
+    with open("stopwords_cn.txt", 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            stop_word.append(line.strip())
+    for line in df['fenci']:
+        line = [str(word).strip(' ') for word in line.split(' ') if len(word) >= 2 and word not in stop_word]
+        train.append(line)
+
+    # 构建为字典的格式
+    dictionary = corpora.Dictionary(train)
+    corpus = [dictionary.doc2bow(text) for text in train]
+
+    if not os.path.exists("./扇贝-lda"):
+        os.mkdir("./扇贝-lda")
+
+    num_topics = 2
+    # LDA可视化模块
+    # 构建lda主题参数
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics, random_state=111,
+                                          iterations=400)
+    # 读取lda对应的数据
+    data1 = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
+    # 把数据进行可视化处理
+    pyLDAvis.save_html(data1, './扇贝-lda/扇贝-lda(一级维度).html')
+
+    # 主题判断模块
+    list3 = []
+    list2 = []
+    # 这里进行lda主题判断
+    for i in lda.get_document_topics(corpus)[:]:
+        listj = []
+        list1 = []
+        for j in i:
+            list1.append(j)
+            listj.append(j[1])
+        list3.append(list1)
+        bz = listj.index(max(listj))
+        list2.append(i[bz][0])
+
+    df['一级-主题概率'] = list3
+    df['一级-主题类型'] = list2
+
+    df.to_excel('./扇贝-lda/扇贝-lda_data(一级维度).xlsx', index=False)
+
+    data = df
+    # 获取对应主题出现的频次
+    new_data = data['一级-主题类型'].value_counts()
+    new_data = new_data.sort_index(ascending=True)
+    y_data1 = [y for y in new_data.values]
+
+    # 主题词模块
+    word = lda.print_topics(num_words=30)
+    topic = []
+    quanzhong = []
+    list_gailv = []
+    list_gailv1 = []
+    list_word = []
+    # 根据其对应的词，来获取其相应的权重
+    for w in word:
+        ci = str(w[1])
+        c1 = re.compile('\*"(.*?)"')
+        c2 = c1.findall(ci)
+        list_word.append(c2)
+        c3 = '、'.join(c2)
+
+        c4 = re.compile(".*?(\d+).*?")
+        c5 = c4.findall(ci)
+        for c in c5[::1]:
+            if c != "0":
+                gailv = str(0) + '.' + str(c)
+                list_gailv.append(gailv)
+        list_gailv1.append(list_gailv)
+        list_gailv = []
+        zt = "Topic" + str(w[0])
+        topic.append(zt)
+        quanzhong.append(c3)
+
+    list_Attention = []
+    for k in list_gailv1:
+        list_k = [float(k1) for k1 in k]
+        Attention = sum(list_k) / len(list_k)
+        list_Attention.append(Attention)
+
+
+    y_data2 = []
+    for y in y_data1:
+        number = float(y / sum(y_data1))
+        y_data2.append(float('{:0.5}'.format(number)))
+
+    df1 = pd.DataFrame()
+    df1['所属主题'] = topic
+    df1['文本数量'] = y_data1
+    df1['特征词'] = quanzhong
+    df1['维度关注度'] = list_Attention
+    df1['内部占比'] = y_data2
+    df1.to_excel('./扇贝-lda/特征维度(一级维度).xlsx', index=False)
+
+
+if __name__ == '__main__':
+    df = pd.read_excel('新_扇贝_sentiment.xlsx')
+    lda(df)
