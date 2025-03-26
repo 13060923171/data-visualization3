a63f8e7d1ef867f84837aa97d18c5cc26be198d7
有猫腻妖 <960751327@qq.com>
2025-03-26 09:39:35 +0800

    项目更新

---
+++ 数据挖掘实战-中文数据 lda 词频词云 百度情感分析/4、情感分析.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from paddlenlp import Taskflow
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')  # 使用Agg后端
+
+
+def emotion_analysis(text):
+    # 进行情感分析
+    results = sentiment_analysis(text)
+    for result in results:
+        label = result['label']
+        score = result['score']
+        if label == 'negative' and score <= 0.3:
+            label_class = '轻度消极'
+        elif label == 'negative' and 0.3 < score <= 0.7:
+            label_class = '消极'
+        elif label == 'negative' and 0.7 < score:
+            label_class = '强烈消极'
+        elif label == 'positive' and score <= 0.3:
+            label_class = '轻度积极'
+        elif label == 'positive' and 0.3 < score <= 0.7:
+            label_class = '积极'
+        elif label == 'positive' and 0.7 < score:
+            label_class = '强烈消极'
+        return label,score,label_class
+
+
+df = pd.read_csv('./lda/lda_data.csv')
+# 初始化情感分析任务
+sentiment_analysis = Taskflow("sentiment_analysis")
+list_label = []
+list_score = []
+list_class = []
+for d in df['fenci']:
+    label,score,label_class = emotion_analysis(d)
+    list_label.append(label)
+    list_score.append(score)
+    list_class.append(label_class)
+df['label'] = list_label
+df['score'] = list_score
+df['class'] = list_class
+df.to_excel('情感分析.xlsx')
+
+def emotion_pie(df1,name):
+    new_df = df1['class'].value_counts()
+    x_data = [x for x in new_df.index]
+    y_data = [y for y in new_df.values]
+    plt.figure(figsize=(9, 6), dpi=500)
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.pie(y_data, labels=x_data, startangle=0, autopct='%1.2f%%')
+    plt.title(f'{name}-情感占比分布')
+    plt.tight_layout()
+    # 添加图例
+    plt.legend(x_data, loc='lower right')
+    plt.savefig(f'{name}-情感占比分布.png')
+
+
+list_number = [[0,2,9,10,13],[1,4,6,12],[3,7,11],[5,8,11]]
+list_name =['价格诚信','物流售后','平台责任','商品质量']
+for n1,n2 in zip(list_number,list_name):
+    df1 = df[df['主题类型'].isin(n1)]
+    emotion_pie(df1,n2)
+
